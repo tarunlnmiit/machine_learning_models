039169b59139e4a94d34f9480c613667280dd99e
Tarun Gupta <tarungupta.y12@lnmiit.ac.in>
2019-11-01 12:41:07 +0100

    Final batch gradient descent program

--- Assignment 1/linearregr.py
+++ Assignment 1/linearregr.py
@@ -1,11 +1,24 @@
+#!/usr/bin/env python3
+"""
+[Monday 13-15] ML Programming Assignment 1
+Tarun Gupta, Shipra Dureja
+
+Command to run this batch gradient descent model:
+python3 linearregr.py --data <filepath> --learningRate <learning Rate> --threshold <Threshold Value>
+
+Input: csv File
+Output: csv File
+Output File naming convention: "solution_<input csv name>_learningRate_<learning Rate value>_threshold_<threshold value>.csv"
+Output File Format(rounded to 4 decimal places): iteration_number,weight0,weight1,weight2,...,weightN,sum_of_squared_errors
+
+The output file generated will be present in the same directory as this python file (that is parallel to this file)
+"""
 import argparse
 import csv
-from decimal import Decimal
 import numpy as np
 
 
 def main():
-    # Shiprs's github profile -- ShipraDureja
     args = parser.parse_args()
     file, learningRate, threshold = args.data, float(
         args.learningRate), float(args.threshold)
@@ -14,48 +27,62 @@ def main():
         X = []
         Y = []
         for row in reader:
-            X.append([1] + row[:-1])
-            Y.append(row[-1])
+            X.append([1.0] + row[:-1])
+            Y.append([row[-1]])
 
     n = len(X)
     X = np.array(X).astype(float)
     Y = np.array(Y).astype(float)
-    W = np.zeros(len(X[0])).astype(float)
-    # W[0] = 1
+    W = np.zeros(X.shape[1]).astype(float)
+    W = W.reshape(X.shape[1], 1).round(4)
 
-    """ Feature Scaling
-    mean = np.ones(X.shape[1] - 1)
-    std = np.ones(X.shape[1] - 1)
+    f_x = calculatePredicatedValue(X, W)
+    sse_old = calculateSSE(Y, f_x)
 
-    for i in range(1, X.shape[1]):
-        mean[i-1] = np.mean(X.transpose()[i])
-        std[i-1] = np.std(X.transpose()[i])
-        for j in range(X.shape[0]):
-            X[j][i] = (X[j][i] - mean[i-1]) / std[i-1]
-    """
+    outputFile = 'solution_' + \
+                 'learningRate_' + str(learningRate) + '_threshold_' \
+                 + str(threshold) + '.csv'
 
-    f_x = np.ones(X.shape[0])
-    sse = 'inf'
-    iteration = 1
-    while sse == 'inf' or threshold < sse:
-        for i in range(n):
-            f_x[i] = float(np.matmul(W, X[i]))
+    with open(outputFile, 'w', newline='') as csvFile:
+        writer = csv.writer(csvFile, delimiter=',', quoting=csv.QUOTE_NONE, escapechar='')
+        writer.writerow([*[0], *["{0:.4f}".format(val) for val in W.T[0]], *["{0:.4f}".format(sse_old)]])
 
-        sse = np.sum(np.square(Y - f_x))
-        # sse = (1 / X.shape[0]) * 0.5 * sum(np.square(Y - f_x))
-        sse = float("{0:.4f}".format(sse))
-        gradient = np.ones((X.shape[0], len(X[0])))
-        for i in range(n):
-            gradient[i] = (Y[i] - f_x[i]) * X[i]
+        gradient, W = calculateGradient(W, X, Y, f_x, learningRate)
 
-        print("{}, {}, {}" .format(iteration, ', '.join(
-            ["{0:.4f}".format(val) for val in W]), "{0:.4f}".format(sse)))
+        iteration = 1
+        while True:
+            f_x = calculatePredicatedValue(X, W)
+            sse_new = calculateSSE(Y, f_x)
 
-        gradient = np.sum(gradient, axis=0)
-        W += learningRate * gradient
-        W = np.array([float("{0:.4f}".format(val)) for val in W])
+            if abs(sse_new - sse_old) > threshold:
+                writer.writerow([*[iteration], *["{0:.4f}".format(val) for val in W.T[0]], *["{0:.4f}".format(sse_new)]])
+                gradient, W = calculateGradient(W, X, Y, f_x, learningRate)
+                iteration += 1
+                sse_old = sse_new
+            else:
+                break
+        writer.writerow([*[iteration], *["{0:.4f}".format(val) for val in W.T[0]], *["{0:.4f}".format(sse_new)]])
+    print("Output File Name: " + outputFile)
 
-        iteration += 1
+
+def calculateGradient(W, X, Y, f_x, learningRate):
+    gradient = (Y - f_x) * X
+    gradient = np.sum(gradient, axis=0)
+    # gradient = np.array([float("{0:.4f}".format(val)) for val in gradient])
+    temp = np.array(learningRate * gradient).reshape(W.shape)
+    W = W + temp
+    return gradient, W
+
+
+def calculateSSE(Y, f_x):
+    sse = np.sum(np.square(f_x - Y))
+
+    return sse
+
+
+def calculatePredicatedValue(X, W):
+    f_x = np.dot(X, W)
+    return f_x
 
 
 if __name__ == '__main__':
